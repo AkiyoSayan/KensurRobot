ff6582757f7ce70c84d402580b6d6678c88e1ea6
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2021-12-10 10:03:30 +0530

    Kek

--- Lumine/modules/admin.py
+++ Lumine/modules/admin.py
@@ -16,6 +16,8 @@ from Lumine.modules.helper_funcs.chat_status import (
     ADMIN_CACHE,
 )
 
+from Lumine.modulea.helper_funcs.anonymous import user_admin
+
 from Lumine.modules.helper_funcs.telethn.admin_rights import (
     user_can_pin,
     user_can_promote,
