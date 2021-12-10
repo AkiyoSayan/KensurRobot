4b05047b6f2652abb925124c48f525694a8fb09a
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2021-12-10 09:38:58 +0530

    UwU

--- Lumine/modules/helper_funcs/decorators.py
+++ Lumine/modules/helper_funcs/decorators.py
@@ -1,7 +1,7 @@
 from Lumine.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
 from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, InlineQueryHandler
 from telegram.ext.filters import BaseFilter
-from Lumine import dispatcher as d, log
+from Lumine import dispatcher as d, LOGGER as log
 from typing import Optional, Union, List
 
 
